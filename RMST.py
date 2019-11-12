2108b4898e6fe1c7b27ddd2a9bdd48cf84a17d0f
alexis arnaudon <alexis.arnaudon@gmail.com>
2019-11-12 08:24:42 +0100

    license

--- RMST.py
+++ RMST.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+# This file is part of RMST.
+#
+# Copyright (C) 2019, 
+# Alexis Arnaudon (alexis.arnaudon@epfl.ch), 
+#https://github.com/FlorianSong/RelaxedMinimumSpanningTree
+#
+# RMST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RMST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RMST.  If not, see <http://www.gnu.org/licenses/>.
+
 import numpy as np
 import networkx as nx
 from multiprocessing import Pool
